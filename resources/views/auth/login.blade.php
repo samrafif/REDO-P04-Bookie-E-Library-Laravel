eea072e2d788a5586f5b800ccaf9b038509d0dfa
Samwoof <abrisam.rm@gmail.com>
2025-01-22 08:14:00 +0700

    Fixed all the titles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Bookie | Login</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])</head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
     <div class="bg-white p-8 rounded-lg shadow-md w-96">
